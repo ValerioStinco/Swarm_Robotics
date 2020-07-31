99c66923d2b3a58bcf076a09408bdd413e3df5aa
Lenovo110 <stinco.1395639@studenti.uniroma1.it>
2020-08-01 01:03:37 +0200

    must change random walk for DHTF experiments

--- src/examples/behaviors/kilobot_ALF_cre.c
+++ src/examples/behaviors/kilobot_ALF_cre.c
@@ -1,6 +1,9 @@
 #include "kilolib.h"
 #include <stdlib.h>
 #include<stdio.h>
+#include<math.h>
+#include "distribution_functions.c"
+
 
 typedef enum {  // Enum for different motion types
     TURN_LEFT = 1,
@@ -29,7 +32,7 @@ motion_t current_motion_type = STOP;            // Current motion type
 
 action_t current_state = RANDOM_WALKING;        // Current state
 
-uint32_t last_turn_ticks = 0;                   // Counters for motion, turning and random_walk
+/*uint32_t last_turn_ticks = 0;                   // Counters for motion, turning and random_walk
 uint32_t turn_ticks = 60;
 unsigned int turning_ticks = 0;
 const uint8_t max_turning_ticks = 160;          // Constant to allow a maximum rotation of 180 degrees with \omega=\pi/5
@@ -37,6 +40,17 @@ const uint16_t max_straight_ticks = 320;        // Set the \tau_m period to 2.5 
 uint32_t last_motion_ticks = 0;
 uint32_t turn_into_random_walker_ticks = 160;   // Timestep to wait without any direction message before turning into random_walker
 uint32_t last_direction_msg = 0;
+*/
+/* ---------------------------------------------- */
+const float std_motion_steps = 20*16; // variance of the gaussian used to compute forward motion
+const float levy_exponent = 2; // 2 is brownian like motion (alpha)
+const float  crw_exponent = 0.0; // higher more straight (rho)
+uint32_t turning_ticks = 0; // keep count of ticks of turning
+const uint8_t max_turning_ticks = 80; /* constant to allow a maximum rotation of 180 degrees with \omega=\pi/5 */
+unsigned int straight_ticks = 0; // keep count of ticks of going straight
+const uint16_t max_straight_ticks = 320;
+uint32_t last_motion_ticks = 0;
+/* ---------------------------------------------- */
 
 int sa_type = 0;                                //Variables for Smart Arena messages
 int sa_payload = 0;
@@ -171,7 +185,7 @@ void rx_message(message_t *msg, distance_measurement_t *d) {
             if(act_side!=best_side){
                 if((((double) rand() / (RAND_MAX))<directed_motion_freq) && (best_side!=0)){
                     current_state = TURN_TO_TARGET;
-                    printf("TURNO PERCHE BEST_SIDE è %d\n",best_side);
+                    printf("TURN PERCHE BEST_SIDE è %d\n",best_side);
                     printf("INVECE ACT_SIDE è %d\n",act_side);
                 }
             }
@@ -184,7 +198,7 @@ void rx_message(message_t *msg, distance_measurement_t *d) {
             if((best_side==1)){           //VERSO L'ALTO
                 if(((sa_payload>=0)&&(sa_payload<=10))||((sa_payload>=100)&&(sa_payload<=110))){
                     current_state = RANDOM_WALKING;
-                    printf("EEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+                    printf("AVVISO USCITA");
                 }
                 else{
                     if(sa_payload<100){   
@@ -200,7 +214,7 @@ void rx_message(message_t *msg, distance_measurement_t *d) {
             else if((best_side==2)){      //VERSO IL BASSO
                 if(((sa_payload>=20)&&(sa_payload<=31))||((sa_payload>=120)&&(sa_payload<=131))){
                     current_state = RANDOM_WALKING;
-                    printf("EEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+                    printf("AVVISO USCITA");
                 }
                 else{
                     if(sa_payload>100){   
@@ -223,40 +237,46 @@ void rx_message(message_t *msg, distance_measurement_t *d) {
 /*-------------------------------------------------------------------*/
 /* Function implementing the uncorrelated random walk                */
 /*-------------------------------------------------------------------*/
-void random_walk() {
-    switch( current_motion_type ) {
-    case TURN_LEFT:
-        if( kilo_ticks > last_motion_ticks + turning_ticks ) {
-            /* start moving forward */
-            last_motion_ticks = kilo_ticks;  // fixed time FORWARD
-            //last_motion_ticks = rand() % max_straight_ticks + 1;  // random time FORWARD
-            set_motion(FORWARD);
-        }
-    case TURN_RIGHT:
-        if( kilo_ticks > last_motion_ticks + turning_ticks ) {
-            /* start moving forward */
-            last_motion_ticks = kilo_ticks;  // fixed time FORWARD
-            //last_motion_ticks = rand() % max_straight_ticks + 1;  // random time FORWARD
-            set_motion(FORWARD);
-        }
-        break;
-    case FORWARD:
-        if( kilo_ticks > last_motion_ticks + max_straight_ticks ) {
-            /* perform a random turn */
-            last_motion_ticks = kilo_ticks;
-            if( rand()%2 ) {
-                set_motion(TURN_LEFT);
-            }
-            else {
-                set_motion(TURN_RIGHT);
-            }
-            turning_ticks = rand()%max_turning_ticks + 1;
-        }
-        break;
-    case STOP:
-    default:
-        set_motion(STOP);
+void random_walk(){
+  switch (current_motion_type) {
+  case TURN_LEFT:
+  case TURN_RIGHT:
+    /* if turned for enough time move forward */
+    if(kilo_ticks > last_motion_ticks + turning_ticks) {
+      /* start moving forward */
+      last_motion_ticks = kilo_ticks;
+      set_motion(FORWARD);
     }
+    break;
+  case FORWARD:
+    /* if moved forward for enough time turn */
+    if(kilo_ticks > last_motion_ticks + straight_ticks) {
+      float angle = 0; // rotation angle
+
+      /* perform a random turn */
+      last_motion_ticks = kilo_ticks;
+      if (rand_soft() % 2) {
+        set_motion(TURN_LEFT);
+      } else {
+        set_motion(TURN_RIGHT);
+      }
+      /* random angle */
+      if(crw_exponent == 0) {
+        angle = uniform_distribution(0, (M_PI));
+      } else {
+        angle = fabs(wrapped_cauchy_ppf(crw_exponent));
+      }
+
+      /* compute turning time */
+      turning_ticks = (uint32_t)((angle / M_PI) * max_turning_ticks);
+      straight_ticks = (uint32_t)(fabs(levy(std_motion_steps, levy_exponent)));
+    }
+    break;
+
+  case STOP:
+  default:
+    set_motion(FORWARD);
+  }
 }
 
 
